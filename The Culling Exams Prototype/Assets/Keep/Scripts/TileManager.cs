55fc2384a26a26e65f136890040443b82dc499fc
keepcarter <keepcarter@yahoo.com>
2019-04-13 14:02:59 -0400

    Adding Keep Folder and Level 1 prototype

---
+++ The Culling Exams Prototype/Assets/Keep/Scripts/TileManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public int rows;
+    public int columns;
+
+    public GameObject tilePrefab;
+
+    private GameObject[,] allTiles;
+    public TileMap tileMap;
+
+    // Map Editor Materials
+    public Material defaultMaterial;
+    public Material startMaterial;
+    public Material finishMaterial;
+    public Material barrierMaterial;
+    public Material fallMaterial;
+    public Material moveableMaterial;
+
+    public Material crateMaterial;
+    public Material mirrorMaterial;
+
+    // Dynamic Materials
+    public Material normalMaterial;
+    public Material highlightMaterial;
+    public Material currentMaterial;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        allTiles = new GameObject[rows, columns];
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                allTiles[row, column] = Instantiate(tilePrefab, new Vector3(row,0,column), this.transform.rotation, this.transform);
+                allTiles[row, column].GetComponent<Tile>().Row = row;
+                allTiles[row, column].GetComponent<Tile>().Column = column;
+            }
+        }
+        
+        // If the Scriptable Object for this level does not have the same rows columns lets go ahead and adjust it
+        if (tileMap != null && (tileMap.rowCount != rows || tileMap.columnCount != columns))
+        {
+            tileMap.rowCount = rows;
+            tileMap.columnCount = columns;
+
+            tileMap.rows = new TileMap.Row[rows];
+
+            for( int row = 0; row < rows; row++)
+            {
+                tileMap.rows[row] = new TileMap.Row();
+                tileMap.rows[row].column = new TileMap.Row.Tile[columns];
+            }
+            EditorUtility.SetDirty(tileMap);
+        }
+    }
+
+    private bool editLoop = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (editLoop)
+        {
+
+#if !UNITY_EDITOR // Do the updates in Editor mode so you can edit the maps. Otherwise set the map once after everything is instanciated.
+            editLoop = false;
+#endif
+            SetMapValues();
+        }
+
+        SetDynamicValues();
+    }
+
+    private void SetDynamicValues ()
+    {
+    }
+
+    private void SetMapValues ()
+    {
+        foreach (GameObject tile in allTiles)
+        {
+            Tile scriptedTile = tile.GetComponent<Tile>();
+            MeshRenderer[] meshes = tile.GetComponentsInChildren<MeshRenderer>();
+
+            int row = scriptedTile.Row;
+            int column = scriptedTile.Column;
+
+            TileMap.Row.Tile tileMapTile = tileMap.rows[row].column[column];
+
+            if (tileMapTile.startTile && startMaterial != null)
+                meshes[0].material = startMaterial;
+
+            else if (tileMapTile.finishTile && finishMaterial != null)
+                meshes[0].material = finishMaterial;
+
+            else if (tileMapTile.barrierTile && barrierMaterial != null)
+                meshes[0].material = barrierMaterial;
+
+            else if (tileMapTile.fallTile && fallMaterial != null)
+                meshes[0].material = fallMaterial;
+
+            else
+                meshes[0].material = defaultMaterial;
+            
+            transform.rotation = Quaternion.Euler(tileMapTile.rotation);
+            if (tileMapTile.Crate)
+            {
+                meshes[1].enabled = true;
+                meshes[1].material = crateMaterial;
+            }
+            else if (tileMapTile.Mirror)
+            {
+                meshes[1].enabled = true;
+                meshes[1].material = mirrorMaterial;
+            }
+            else
+                meshes[1].enabled = false;
+        }
+    }
+}
---
+++ The Culling Exams Prototype/Assets/Keep/Scripts/TileMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level1", menuName = "TileMap/Levels", order = 1)]
+public class TileMap : ScriptableObject
+{
+    public Row[] rows;
+
+    public int rowCount;
+    public int columnCount;
+
+    [Serializable]
+    public class Row
+    {
+        public Tile[] column;
+
+        [Serializable]
+        public class Tile
+        {
+            public bool fallTile = false;
+            public bool barrierTile = false;
+            public bool startTile = false;
+            public bool finishTile = false;
+            public bool moveableTile = false;
+
+            public bool Crate = false;
+            public bool Mirror = false;
+            public Vector3 rotation;
+        }
+    }
+}
+
