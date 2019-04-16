f0196d234b8abcacefadb1ff268a0d7b072bc0e4
Philip Carter <keepcarter@yahoo.com>
2019-04-16 10:10:20 -0400

    Update TilePlayManager.cs

--- The Culling Exams Prototype/Assets/Keep/Scripts/TilePlayManager.cs
+++ The Culling Exams Prototype/Assets/Keep/Scripts/TilePlayManager.cs
@@ -37,34 +37,34 @@ public class TilePlayManager : TileManagerBase
         }
 
         // Objects
-        allCrates = new List<GameObject>();
-        for (int row = 0; row < rows; row++)
-        {
-            for (int column = 0; column < columns; column++)
-            {
-                TileMap.Row.Tile tile = tileMap.rows[row].column[column];
+        //allCrates = new List<GameObject>();
+        //for (int row = 0; row < rows; row++)
+        //{
+        //    for (int column = 0; column < columns; column++)
+        //    {
+        //        TileMap.Row.Tile tile = tileMap.rows[row].column[column];
 
-                if (tile.crateType != TileMap.CrateType.none)
-                {
+        //        if (tile.crateType != TileMap.CrateType.none)
+        //        {
 
-                    GameObject addMe = Instantiate(
-                            cratePrefab, tile.CenterPoint,
-                            Quaternion.Euler(tile.rotation),
-                            this.transform);
+        //            GameObject addMe = Instantiate(
+        //                    cratePrefab, tile.CenterPoint,
+        //                    Quaternion.Euler(tile.rotation),
+        //                    this.transform);
 
-                    MeshRenderer[] meshes = addMe.GetComponentsInChildren<MeshRenderer>();
+        //            MeshRenderer[] meshes = addMe.GetComponentsInChildren<MeshRenderer>();
 
-                    if (tile.crateType == TileMap.CrateType.crate)
-                        meshes[0].material = crateMaterial;
-                    else if (tile.crateType == TileMap.CrateType.mirror)
-                        meshes[0].material = mirrorMaterial;
-                    else
-                        meshes[0].material = defaultMaterial;
+        //            if (tile.crateType == TileMap.CrateType.crate)
+        //                meshes[0].material = crateMaterial;
+        //            else if (tile.crateType == TileMap.CrateType.mirror)
+        //                meshes[0].material = mirrorMaterial;
+        //            else
+        //                meshes[0].material = defaultMaterial;
 
-                    allCrates.Add(addMe);
-                }
-            }
-        }
+        //            allCrates.Add(addMe);
+        //        }
+        //    }
+        //}
 
         for (int row = 0; row < rows; row++)
         {
@@ -84,5 +84,26 @@ public class TilePlayManager : TileManagerBase
     void Update ()
     {
         SetDynamicValues();
+
+        Vector3 playerPosition = this.transform.GetChild(0).transform.position;
+        foreach (GameObject tile in allTiles)
+        {
+            // If the player is on this tile
+
+            Tile scriptedTile = tile.GetComponent<Tile>();
+            if (scriptedTile.Row + 0.5f > playerPosition.x && scriptedTile.Row - 0.5f > playerPosition.x
+                && scriptedTile.Column + 0.5f > playerPosition.y && scriptedTile.Column - 0.5f > playerPosition.y)
+            {
+                // This will need to be turned into a switch
+                
+                TileMap.Row.Tile dataTile = tileMap.rows[scriptedTile.Row].column[scriptedTile.Column];
+                if (dataTile.tileType == TileMap.TileType.finish)
+                {
+                    this.transform.GetChild(0).transform.SetPositionAndRotation(dataTile.CenterPoint, Quaternion.identity);
+                }
+            }
+        }
+
+
     }
 }
