9d3171fb68e8114646e5dc39749ccfaf248a41a4
Yi Li <plumeliiii@gmail.com>
2019-04-16 10:43:28 -0400

    Finished Win and Lose Scene
     99%, Need to improve logic

---
+++ The Culling Exams Prototype/Assets/Yi/Scripts/SceneManagerSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerSystem : MonoBehaviour
+{
+    public string[] LevelsName;
+    private bool isMainMenu = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == LevelsName[0])
+            isMainMenu = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMainMenu)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(LevelsName[1]);
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(LevelsName[1]);
+    }
+}
