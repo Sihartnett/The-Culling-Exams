db814ee8553dfbe91cfef58a7ee57d38bd98f324
Spence Hartnett <sihartnett8@fullsail.edu>
2019-04-16 10:47:09 -0400

    Added prudvhi's package

---
+++ The Culling Exams Prototype/Assets/Prudhvi/ClickOn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    [SerializeField]
+    public Material red;
+    [SerializeField]
+    public Material green;
+
+    private MeshRenderer myRend;
+    [HideInInspector]
+    public bool currentlySelected = false;
+    void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+    }
+    
+    public void Clickme()
+    {
+        if(currentlySelected == false)
+        {
+            myRend.material = red;
+        }
+        else
+        {
+            myRend.material = green;
+
+        }
+    }
+    
+}
