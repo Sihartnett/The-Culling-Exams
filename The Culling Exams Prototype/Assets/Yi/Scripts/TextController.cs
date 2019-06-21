21812e47a16393cc8c2319459606b620e2181b92
Yi Li <plumeliiii@gmail.com>
2019-06-21 15:19:54 -0400

    pushed Narrtive

---
+++ The Culling Exams Prototype/Assets/Yi/Scripts/TextController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextController : MonoBehaviour
+{
+    [SerializeField] TextAsset dialogue;
+    [SerializeField] TextMeshProUGUI outputText;
+
+    [SerializeField] string Character1;
+    [SerializeField] string Character2;
+
+    [SerializeField] RawImage C1;
+    [SerializeField] RawImage C2;
+
+    private string[] text;
+
+    private int counter = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeText();
+        NextLineOfDialogue();
+    }
+
+    void InitializeText()
+    {
+        text = dialogue.text.Split('\n');
+    }
+
+    public void NextLineOfDialogue()
+    {
+        if (counter < text.Length - 1)
+        {
+            counter++;
+            outputText.text = text[counter];
+            SetUpPortrait(counter);
+        }
+        else
+            Debug.Log("No text!");
+    }
+
+    void SetUpPortrait(int count)
+    {
+        if (text[count].Contains(Character1)) {
+            Debug.Log(text[count]);
+            C1.color = new Color32(255, 255, 255, 255);
+            C2.color = new Color32(128, 128, 128, 255);
+        }
+        else if (text[count].Contains(Character2)) {
+            C1.color = new Color32(128, 128, 128, 255);
+            C2.color = new Color32(255, 255, 255, 255);
+            Debug.Log(text[count]);
+        }
+        }
+}
